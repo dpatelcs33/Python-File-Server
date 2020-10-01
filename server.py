46455f8f8f95e0c35c5154237f100305dfaf3533
Dhaval <dpatelcs33@gmail.com>
2020-10-01 17:20:30 -0400

    async: fixed headers and support for ETA

--- server.py
+++ server.py
@@ -8,18 +8,26 @@ class FileHandler(web.RequestHandler):
 
     async def get(self):
 
-        # TODO : handle get requests Asynchronously
+        # TODO : non-blocking file reads
 
         abs_path = os.path.abspath(self.get_argument('path'))
-        print(abs_path)
+        
+        #print(abs_path)
 
         if not os.access(abs_path, os.R_OK):
-            raise web.HTTPError(status_code=404, reason="File Not Found or Access Denied")
+            raise web.HTTPError(status_code=404, reason="File Not Found or File Access Denied")
     
+        file_size = os.path.getsize(abs_path)
         content_type, _ = mimetypes.guess_type(abs_path)
-        print(content_type)
-        self.add_header('Content-Type', content_type)
 
+        if not content_type:
+            self.set_header('Content-Type', "application/octet-stream")
+            
+        #print(content_type)
+
+        self.add_header('Content-Disposition', "attachment; filename={}".format(os.path.basename(abs_path)))
+        self.set_header('Content-Type', content_type)
+        self.add_header('Content-Length', file_size)
         buff_size = 1024 * 1024 * 1
 
         with open(abs_path, "rb") as fp:
@@ -41,6 +49,8 @@ class FileHandler(web.RequestHandler):
                 finally:
                     del chunk
                     await asyncio.sleep(0)
+
+
 def main():
 
     # TODO: production --> debug off , autoreload = off
@@ -48,7 +58,7 @@ def main():
 
         (r"/files", FileHandler)
 
-    ], autoreload = False, debug = False)
+    ], autoreload = True, debug = True)
 
     http_server = httpserver.HTTPServer(app)
     http_server.listen(8080)
